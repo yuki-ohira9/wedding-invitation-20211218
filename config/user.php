d6c603f96ac687b339215a4ea062df79692f5656
yuki ohira private <>
2021-05-30 14:02:38 +0900

    Add: Seeder

---
+++ config/user.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'default_user' => [
+        'name' => '管理用ユーザー',
+        'email' => env('DEFAULT_USER_EMAIL'),
+        'password' => env('DEFAULT_USER_PASSWORD'),
+    ],
+];
