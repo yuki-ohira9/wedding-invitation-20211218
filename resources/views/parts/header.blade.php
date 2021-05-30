82c5a51dbd83ee83ec5ffcc6a16fbe011da814fb
yuki ohira private <>
2021-05-30 21:48:52 +0900

    Update: アニメーション

--- resources/views/parts/header.blade.php
+++ resources/views/parts/header.blade.php
@@ -1,5 +1,9 @@
 <div id="wrapper">
-  <div id="mainBanner">
-    <img src="{{ url('/image/header.jpg') }}" alt="新婦自作フレーム"><div class="slogan"><h2>Wedding Invitation</h2><p>Yuki & Rikako</p><p>December 18th</p>
+  <div class="anim-box" id="mainBanner">
+    <img src="{{ url('/image/header.jpg') }}" alt="新婦自作フレーム">
+    <div class="slogan">
+      <h2 class="write-anime" id="invitation-title">Wedding Invitation</h2>
+      <p class="write-anime" id="invitation-description1">Yuki & Rikako</p>
+      <p class="write-anime" id="invitation-description2">December 18th</p>
   </div>
 </div>
